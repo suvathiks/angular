c153b5ced0bc5c3fc6f89cb8995efdd9bb58cca2
Suvathipriya Duraiswamy <suvathipriya.d@ishafoundation.org>
2018-10-05 10:38:01 +0530

    First Commit

---
+++ server.js
@@ -0,0 +1,16 @@
+// Install Express Server
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+//serve only the static files form the dist directory
+//Replace the '/dist/<to_your_project_name>'
+app.use(express.static(__dirname + '/dist/angular'));
+
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/angular/index.html'));
+});
+
+app.listen(process.env.port || 8080);

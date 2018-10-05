d75b7feaa2373703aa2144af042c06ca47871077
Suvathipriya Duraiswamy <suvathipriya.d@ishafoundation.org>
2018-10-05 11:51:50 +0530

    my commit

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-// Install Express Server
-
+//Install express server
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
-//serve only the static files form the dist directory
-//Replace the '/dist/<to_your_project_name>'
+// Serve only the static files form the dist directory
+// Replace the '/dist/<to_your_project_name>'
 app.use(express.static(__dirname + '/dist/angular'));
 
-app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/angular/index.html'));
+app.get('*', function(req,res) {
+  // Replace the '/dist/<to_your_project_name>/index.html'
+  res.sendFile(path.join(__dirname + '/dist/angular/index.html'));
 });
-
-app.listen(process.env.port || 8080);
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
